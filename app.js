a787d7db36e8e449f3f3a960f19c8f06791d2b0d
Preet Sojitra <preet.dev373@gmail.com>
2023-04-21 21:42:16 +0530

    feat: add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("express-async-errors")
 
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 const connectDB = require("./db/connect")
 const productsRouter = require("./routes/products")
@@ -13,6 +14,8 @@ const errorMiddleware = require("./middleware/error-handler")
 
 // middleware
 app.use(express.json())
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("<h1>Store API </h1> <a href='/api/v1/products'>products</a>")
